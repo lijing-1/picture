cf2c4c2eee658bf61e1b03edc7e50ecfff74fe9c
lijing-1 <54490071+lijing-1@users.noreply.github.com>
2026-08-10 17:46:14 +0800

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,8 @@ from datetime import datetime, timedelta
 now = datetime.now()
 t1 = now + timedelta(hours=1)
 t2 = now - timedelta(hours=1)
-if now.hour >5:
-    print(f"+1h {t1}")
+if now.hour <5:
+    timestamp_sec = int(now.timestamp())
+    print(f"时间早于5点，时间戳(秒): {timestamp_sec}")
 if now.hour >5:
     print(f"-1h {t2}")
